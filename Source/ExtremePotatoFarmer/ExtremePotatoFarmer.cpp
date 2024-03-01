b2a3ff3b077139dff43ecf39a8247f1b7ad4ed9f
finian1 <finian@craigshouse.net>
2024-03-01 13:41:51 +0000

    Initial commit

---
+++ Source/ExtremePotatoFarmer/ExtremePotatoFarmer.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ExtremePotatoFarmer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ExtremePotatoFarmer, "ExtremePotatoFarmer" );
+
+DEFINE_LOG_CATEGORY(LogExtremePotatoFarmer)
+ 
---
+++ Source/ExtremePotatoFarmer/ExtremePotatoFarmerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ExtremePotatoFarmerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AExtremePotatoFarmerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AExtremePotatoFarmerGameMode();
+};
+
+
+
